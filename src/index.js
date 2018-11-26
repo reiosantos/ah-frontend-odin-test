c55bc9324176fab72e1f5473fadb553b4278b58d
Ahimbisibwe Roland <rolandmbasa@gmail.com>
2018-11-26 19:06:54 +0300

    chore(setup) setup SASS preprocessor
    [finishes #161340135]

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import './index.css';
 import App from 'containers/App';
-
 import * as serviceWorker from './serviceWorker';
+import 'sass/app.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
